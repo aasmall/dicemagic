964146abae6f4a168f9f374d177ab9dfdcb51939
Aaron <aaron@smallnet.org>
2018-04-21 16:00:37 -0700

    Added Slack Bot secret to Cloud KMS and built decryption logic

--- roll.go
+++ roll.go
@@ -3,16 +3,17 @@ package main
 import (
 	"crypto/rand"
 	"fmt"
+	"google.golang.org/appengine"
 	"math/big"
 	"net/http"
-	"strings"
+	"regexp"
 	"strconv"
-    "google.golang.org/appengine"
-    "regexp"
+	"strings"
 )
 
 const UintBytes = 2
-var diceRegexp = regexp.MustCompile(`(?i)^\/(\d+)d(\d+)$`)
+
+var diceRegexp = regexp.MustCompile(`(?i)^\/*(\d+)d(\d+)$`)
 
 func main() {
 	//minPtr := flag.Int("min", 1, "min value")
@@ -20,28 +21,29 @@ func main() {
 	//flag.Parse()
 	//fmt.Println("I rolled", roll(*minPtr,*maxPtr))
 	http.HandleFunc("/", handle)
+	http.HandleFunc("/slack/roll/", slackRoll)
+	http.HandleFunc("/slack/roll", slackRoll)
 	appengine.Main()
 }
-func handle(w http.ResponseWriter, r *http.Request) { 
+func handle(w http.ResponseWriter, r *http.Request) {
 	//numberOfDice, sides:=parseDice(r.URL.Path)
 	content := r.URL.Path
 	if !diceRegexp.MatchString(content) {
-		fmt.Fprintf(w,"%s is not a valid roll\n", strings.Replace(content, "/","",-1))
+		fmt.Fprintf(w, "%s is not a valid roll\n", strings.Replace(content, "/", "", -1))
 		return
 	}
-	numberOfDice,_:=strconv.ParseInt(diceRegexp.FindStringSubmatch(content)[1],10,0)
-	sides,_:=strconv.ParseInt(diceRegexp.FindStringSubmatch(content)[2],10,0)
+	numberOfDice, _ := strconv.ParseInt(diceRegexp.FindStringSubmatch(content)[1], 10, 0)
+	sides, _ := strconv.ParseInt(diceRegexp.FindStringSubmatch(content)[2], 10, 0)
 	rollResult := roll(int(numberOfDice), int(sides))
-	diceWord:=""
-	if int(numberOfDice)>1 {
-		diceWord="dice"
-	}else{
-		diceWord="die"
+	diceWord := ""
+	if int(numberOfDice) > 1 {
+		diceWord = "dice"
+	} else {
+		diceWord = "die"
 	}
 
 	fmt.Fprintf(w, "You rolled %d on %d %d sided %s\n", rollResult, numberOfDice, sides, diceWord)
 }
-
 
 func GenerateRandomInt(min int, max int) int64 {
 	size := max - min + 1
