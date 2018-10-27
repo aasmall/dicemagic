0eb53e7d1f962380795e27ba60d23780a057afdb
Aaron Small <aaron@smallnet.org>
2018-10-27 03:42:56 -0700

    fixed /slash/roll

--- app/chat-clients/slack-slashroll.go
+++ app/chat-clients/slack-slashroll.go
@@ -4,13 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/aasmall/dicemagic/app/dicelang/errors"
 	"github.com/aasmall/dicemagic/app/handler"
-	pb "github.com/aasmall/dicemagic/app/proto"
 	"github.com/nlopes/slack"
-	"golang.org/x/net/context"
 )
 
 func SlackSlashRollHandler(e interface{}, w http.ResponseWriter, r *http.Request) error {
@@ -24,36 +21,29 @@ func SlackSlashRollHandler(e interface{}, w http.ResponseWriter, r *http.Request
 		}
 	}
 
-	log := c.log.WithRequest(r)
-	//read body and reset request
+	//log := c.log.WithRequest(r)
 	s, err := slack.SlashCommandParse(r)
 	if err != nil {
 		fmt.Fprintf(w, "could not parse slash command: %s", err)
 	}
-	rollerClient := pb.NewRollerClient(c.diceClient)
-	timeOutCtx, cancel := context.WithTimeout(r.Context(), time.Second)
-	defer cancel()
-	cmd := s.Text
-	log.Debug(cmd)
-	diceServerResponse, err := rollerClient.Roll(timeOutCtx, &pb.RollRequest{Cmd: cmd, RootOnly: true})
+	rollResponse, err := Roll(c.diceClient, s.Text)
 	if err != nil {
 		c.log.Errorf("Unexpected error: %+v", err)
 		returnErrorToSlack(fmt.Sprintf("Oops! an unexpected error occured: %s", err), w, r)
-		return err
-	}
-
-	if !diceServerResponse.Ok {
-		if diceServerResponse.Error.Code == errors.Friendly {
-			returnErrorToSlack(diceServerResponse.Error.Msg, w, r)
-			return nil
-		}
-		returnErrorToSlack(fmt.Sprintf("Oops! an error occured: %s", diceServerResponse.Error.Msg), w, r)
 		return nil
 	}
-
-	webhookMessage := slack.WebhookMessage{}
-	webhookMessage.Attachments = append(webhookMessage.Attachments, SlackAttachmentsFromRollResponse(diceServerResponse)...)
-
+	if !rollResponse.Ok {
+		if rollResponse.Error.Code == errors.Friendly {
+			returnErrorToSlack(rollResponse.Error.Msg, w, r)
+			return nil
+		} else {
+			returnErrorToSlack(fmt.Sprintf("Oops! an error occured: %s", rollResponse.Error.Msg), w, r)
+			return nil
+		}
+	}
+	webhookMessage := slack.Msg{}
+	webhookMessage.Attachments = append(webhookMessage.Attachments, SlackAttachmentsFromRollResponse(rollResponse)...)
+	c.log.Errorf("Webook Message Sending: %+v", webhookMessage)
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(webhookMessage)
 	return nil
